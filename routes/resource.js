1dbcedf7b3f661183331456b92a2bcd3f5dcc180
Vennela <S544925@nwmissouri.edu>
2021-11-10 23:32:26 -0600

    page to display all

--- routes/resource.js
+++ routes/resource.js
@@ -9,24 +9,24 @@ var jewelry_controller = require('../controllers/jewelry');
 /// API ROUTE /// 
  
 // GET resources base. 
-router.get('/resource', api_controller.api); 
+router.get('/', api_controller.api); 
  
 /// jewelry ROUTES /// 
  
 // POST request for creating a jewelry.  
-router.post('/resource/jewelrys', jewelry_controller.jewelry_create_post); 
+router.post('/jewelrys', jewelry_controller.jewelry_create_post); 
  
 // DELETE request to delete jewelry. 
-router.delete('/resource/jewelrys/:id', jewelry_controller.jewelry_delete); 
+router.delete('/jewelrys/:id', jewelry_controller.jewelry_delete); 
  
 // PUT request to update jewelry. 
-router.put('/resource/jewelrys/:id', 
+router.put('/jewelrys/:id', 
 jewelry_controller.jewelry_update_put); 
  
 // GET request for one jewelry. 
-router.get('/resource/jewelrys/:id', jewelry_controller.jewelry_detail); 
+router.get('/jewelrys/:id', jewelry_controller.jewelry_detail); 
  
 // GET request for list of all jewelry items. 
-router.get('/resource/jewelrys', jewelry_controller.jewelry_list); 
+router.get('/jewelrys', jewelry_controller.jewelry_list); 
  
 module.exports = router;
