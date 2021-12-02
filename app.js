0bd2d1261e7697273f311737be61102a1b3569d9
Vennela <S544925@nwmissouri.edu>
2021-12-01 22:42:55 -0600

    assignment 13

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy;
 
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  } 
+))
 
 const connectionString =  'mongodb+srv://S544925:De%40dp00l@cluster0.cofhu.mongodb.net/learnMongo?retryWrites=true&w=majority'
 mongoose = require('mongoose');
@@ -19,6 +35,59 @@ var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
 const jewelry = require('./models/jewelry');
 var resource = require("./routes/resource");
+
+async function recreateDB(){
+  // Delete everything
+  await jewelry.deleteMany();
+  
+  let instance1 = new jewelry({
+   brand:"SVNR", material:"Gold",
+ cost:"Fifty USD"});
+  instance1.save( function(err,doc) {
+  if(err) return console.error(err);
+  console.log("First object saved")
+  });
+  let instance2 = new jewelry({
+   brand: "Idyl",
+   material: "$Silver",
+   cost: "Six hundred USD"
+ });
+ instance2.save(function (err, doc) {
+   if (err) return console.error(err);
+   console.log("Second object saved")
+ });
+ let instance3 = new jewelry({
+   brand: "Kinn Studio",
+   brand: "Platinum",
+   cost: "Three hundred USD"
+ });
+ instance3.save(function (err, doc) {
+   if (err) return console.error(err);
+   console.log("Third object saved")
+ });
+ }
+ let reseed = true;
+ if (reseed) { recreateDB();}
+ 
+ var app = express();
+
+ // view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+app.use(express.static(path.join(__dirname, 'public')));
+
 //var jewelrydetail = require("./routes/jewelrydetail");
 // We can seed the collection if needed on
 //Get the default connection
@@ -32,40 +101,7 @@ console.log("Connection to DB succeeded");
 
 // We can seed the collection if needed on
 
-async function recreateDB(){
- // Delete everything
- 
- await jewelry.deleteMany();
- let instance1 = new jewelry({
-  brand:"SVNR", material:"Gold",
-cost:"Fifty USD"});
- instance1.save( function(err,doc) {
- if(err) return console.error(err);
- console.log("First object saved")
- });
- let instance2 = new jewelry({
-  brand: "Idyl",
-  material: "$Silver",
-  cost: "Six hundred USD"
-});
-instance2.save(function (err, doc) {
-  if (err) return console.error(err);
-  console.log("Second object saved")
-});
-let instance3 = new jewelry({
-  brand: "Kinn Studio",
-  brand: "Platinum",
-  cost: "Three hundred USD"
-});
-instance3.save(function (err, doc) {
-  if (err) return console.error(err);
-  console.log("Third object saved")
-});
-}
-let reseed = true;
-if (reseed) { recreateDB();}
 
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -83,15 +119,20 @@ app.use('/jewelry', jewelryRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource',resource);
-//app.use('/jewelrydetail',jewelrydetail);
 
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
+ 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -101,6 +142,12 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
-
 module.exports = app;
+
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
--- models/jewelry.js
+++ models/jewelry.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose") 
 const jewelrySchema = mongoose.Schema({ 
  brand: String, 
- material: String, 
+ material: {
+    type: String,
+    minLength: 2
+}, 
  cost: String 
-
 }) 
  
 module.exports = mongoose.model("jewelry", 
